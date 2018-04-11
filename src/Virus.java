afd3621b50072f3bb7634937a07e7aa4a9ef7d9b
Batluca <38223794+Batluca@users.noreply.github.com>
2018-04-11 18:07:49 +0200

    Update Virus.java

--- src/Virus.java
+++ src/Virus.java
@@ -1,4 +1,5 @@
-public class Virus implements Cell, CanEat {
+
+public class Virus extends Cell implements CanEat {
   	private int mass;
   	private double radian;
   	private final double x;
@@ -8,5 +9,20 @@ public class Virus implements Cell, CanEat {
   	public Virus(double x, double y) {
       	this.x = x;
       	this.y = y;
+    }
+
+    private void split(double angle) {
+    	Virus virusClone = new Virus(getX(), getY());
+    	//add new Virus to game-object-list
+    	//split animation
+    }
+    
+    public void eat(EjectedMass cell) {
+    	setMass(getMass()+cell.getMass());
+    	cell.delete();
+    }
+
+    public void delete() {
+    	//remove from game-object-list
     }
 }
