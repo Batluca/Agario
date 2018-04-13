cd427bfd32fc6b8f867e0b15c80f0fac3ae91d6a
Batluca <38223794+Batluca@users.noreply.github.com>
2018-04-13 22:26:12 +0200

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -1,25 +1,39 @@
+import java.util.List;
+import java.util.ArrayList;
+import javafx.scene.paint.Color;
+import javafx.scene.Node;
+import java.util.Collections;
+import javafx.scene.shape.Circle;
+
 public class Player {
   	private int mass;
   	private final String name;
   	private List<PlayerCell> cells = new ArrayList<>();
+  	private Color color;
   
   	public Player(double x, double y, String name) {
       	this.name = name;
+      	color = createColor();
       	createCell(x, y);
     }
   	
   	//methods
   	public void split() {
       	for (PlayerCell cell : cells) {
-          	cell.split()
+          	cell.split();
         }
     }
-  	private createCell(double x, double y) {
-      	cells.append(new PlayerCell(x, y, createColor()));
+  	private void createCell(double x, double y) {
+      	cells.add(new PlayerCell(x, y));
     }
-    private void deleteCell() {
+    private void deleteCell(PlayerCell cell) {
+    	cells.remove(cell);
+    }
 
+    public List<PlayerCell> getCells() {
+    	return cells;
     }
+
   	private static Color createColor() {
 		List<Integer> values = new ArrayList<>(); 
 		values.add(255);
@@ -42,37 +56,44 @@ public class Player {
   	public String getName() {
       	return name;
     }
+
+    public Color getColor() {
+    	return color;
+    }
   	
   	//inner class
-	private class PlayerCell extends Cell implements CanEat {
+	public class PlayerCell extends Cell implements CanEat {
 		
-	  	public PlayerCell(double x, double y, double radian) {
+	  	public PlayerCell(double x, double y, int mass) {
 	      	this.x = x;
 	      	this.y = y;
-	      	this.randian = randian;
+	      	setMass(mass);
 	      	//create cell body
-	      	Circle node = new Circle(x, y, radian, getColor());
+	      	node = new Circle(x, y, radian, getColor());
 	    }
-	    public PlayerCell(double x, double y, int mass) {
-	    	setRadian(mass);
-	      	this(x, y, getRadian());
-	    }
+
 	  	public PlayerCell(double x, double y) {
-	      	this(x, y, Settings.START_MASS);
+	      	this(x, y, Settings.PLAYER_START_MASS);
 	    }
 	  	
 	  	
 	  	//methods
-	  	public void split();
-	  	public void eject();
-	  	public void delete();
+	  	public void split() {
+
+	  	}
+	  	public void eject() {
+
+	  	}
+	  	public void delete() {
+
+	  	}
 	  
 	  	public void eat(Cell cell) {
 	      	
 	    }
 	  
 	  	public boolean canEat(Cell cell) {
-	      	if (getMass() < Math.round(cell.getMass() * 0.1)) {
+	      	if (getMass() > Math.round(cell.getMass() * 1.1)) {
 	          	double distance = Math.sqrt(Math.abs(getX()-cell.getX()) + Math.abs(getY()-cell.getY()));
 	          	if (distance < cell.getRadian()) {
 	              	return true;
@@ -103,9 +124,6 @@ public class Player {
 	    }
 	  	public Node getNode() {
 	      	return node;
-	    }
-	  	public Color getColor() {
-	      	return Color;
 	    }
 	}
 }
